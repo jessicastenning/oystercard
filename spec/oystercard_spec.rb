4d5cd230446261b857cbd90f531fa93961588561
jessica stenning <jessicastenning@hotmail.co.uk>
2018-04-23 15:25:46 +0100

    adds top_up function

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,4 +6,11 @@ describe Oystercard do
       expect(subject).to respond_to :balance
     end
   end
+  describe '#top_up' do
+    it 'should allow user to increase balance by a specified amount' do
+      #expect(subject).to respond_to(:top_up).with(1).arguments
+      subject.top_up(5)
+      expect(subject.balance).to eq 5
+    end
+  end
 end
