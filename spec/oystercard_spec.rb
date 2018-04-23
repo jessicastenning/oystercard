010f09a0e2d5a6f3bc74c6550ae09ae873d4ae73
Salome Lambermont <salome.lambermont@gmail.com>
2018-04-23 15:47:48 +0100

    adds maximum balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,11 +6,15 @@ describe Oystercard do
       expect(subject).to respond_to :balance
     end
   end
+
   describe '#top_up' do
     it 'should allow user to increase balance by a specified amount' do
       #expect(subject).to respond_to(:top_up).with(1).arguments
       subject.top_up(5)
       expect(subject.balance).to eq 5
+    end
+    it 'should raise an error when top up results in balance > £90' do
+      expect{ subject.top_up(91) }.to raise_error 'Balance cannot exceed £90'
     end
   end
 end
