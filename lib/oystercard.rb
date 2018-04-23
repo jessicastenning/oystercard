b5935e2d29557185f74f8192897d96b3423afa23
Salome Lambermont <salome.lambermont@gmail.com>
2018-04-23 15:51:54 +0100

    Refactor

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -6,7 +6,7 @@ class Oystercard
   end
 
   def top_up(amount)
-    raise('Balance cannot exceed £90') if (@balance + amount) > 90
+    raise 'Balance cannot exceed £90' if @balance + amount > 90
     @balance += amount
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,15 +1,8 @@
 require 'oystercard'
 
 describe Oystercard do
-  describe '#balance' do
-    it 'should have a balance' do
-      expect(subject).to respond_to :balance
-    end
-  end
-
   describe '#top_up' do
     it 'should allow user to increase balance by a specified amount' do
-      #expect(subject).to respond_to(:top_up).with(1).arguments
       subject.top_up(5)
       expect(subject.balance).to eq 5
     end
