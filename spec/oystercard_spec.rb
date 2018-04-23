48917e6082420427727748631f8bf62f0a288a5d
Salome Lambermont <salome.lambermont@gmail.com>
2018-04-23 15:05:33 +0100

    Initializes balance (0)

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,9 @@
-desribe Oystercard do
+require 'oystercard'
+
+describe Oystercard do
+  describe '#balance' do
+    it 'should have a balance' do
+      expect(subject).to respond_to :balance
+    end
+  end
 end
